c8b0facf4b95154aab768790ae84c1b79f34e6cc
Pedro Daher <pedrohenriquedaher@icloud.com>
2020-11-22 12:11:10 -0300

    Associates instance with created security group

--- create-ohio-instance.py
+++ create-ohio-instance.py
@@ -2,6 +2,15 @@ import boto3
 
 ec2 = boto3.resource('ec2', region_name='us-east-2')
 
+# Create a security group and allow SSH inbound rule through the VPC
+securitygroup = ec2.create_security_group(
+    GroupName='SSH-PG', Description='allow SSH and PGtraffic')
+securitygroup.authorize_ingress(
+    CidrIp='0.0.0.0/0', IpProtocol='tcp', FromPort=22, ToPort=22)
+securitygroup.authorize_ingress(
+    CidrIp='0.0.0.0/0', IpProtocol='tcp', FromPort=5432, ToPort=5432)
+
+
 # create a new EC2 instance
 instance = ec2.create_instances(
     ImageId='ami-0dd9f0e7df0f0a138',
@@ -9,16 +18,10 @@ instance = ec2.create_instances(
     MaxCount=1,
     InstanceType='t2.micro',
     KeyName='daher-key',
+    SecurityGroupIds=[securitygroup.id],
 )[0]
 
 ec2.create_tags(Resources=[instance.id], Tags=[
                 {'Key': 'Name', 'Value': 'postgres-daher'}, {'Key': 'Creator', 'Value': 'Daher'}])
-
-securitygroup = ec2.SecurityGroup(instance.security_groups[0].get("GroupId"))
-securitygroup.authorize_ingress(
-    CidrIp='0.0.0.0/0', IpProtocol='tcp', FromPort=22, ToPort=22)
-securitygroup.authorize_ingress(
-    CidrIp='0.0.0.0/0', IpProtocol='tcp', FromPort=5432, ToPort=5432)
-
 
 print("Created instance", instance.id)
